1ff69f19f7f42d207782f28ef412cf81b049eb57
fabihakhalid <fabihakhalid2005@gmail.com>
2025-04-18 17:24:37 -0700

    done

---
+++ arrays.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[5] = {12, 45, 23, 67, 34};
+    int max = arr[0];
+
+    for (int i = 1; i < 5; i++) {
+        if (arr[i] > max)
+            max = arr[i];
+    }
+
+    cout << "The largest element is: " << max << endl;
+    return 0;
+}
