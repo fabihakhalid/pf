f8f4ce75c34aa8317d76607e653b95be8d4833f2
Fabiha Khalid <fabihakhalid2005@gmail.com>
2025-04-30 07:47:51 -0700

    Update arrays.cpp

--- arrays.cpp
+++ arrays.cpp
@@ -5,7 +5,7 @@ int main() {
     int arr[5] = {12, 45, 23, 67, 34};
     int max = arr[0];
 
-    for (int i = 1; i < 5; i++) {
+    for (int i = 1; i < 5; i++) { 
         if (arr[i] > max)
             max = arr[i];
     }
